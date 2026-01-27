1fc49a76fc7a1e1a875dd7547e757ca6f26424da
KJ-CORE <kunaljha210@outlook.in>
2026-01-27 22:56:39 +0530

    27/01/2026

--- src/main/java/com/aitpune/Journal/JournalApplication.java
+++ src/main/java/com/aitpune/Journal/JournalApplication.java
@@ -1,10 +1,13 @@
 package com.aitpune.Journal;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -16,6 +19,16 @@ public class JournalApplication {
 
         SpringApplication.run(JournalApplication.class, args);
 	}
+    @Bean
+    public CommandLineRunner checkConfig(
+            @Value("${spring.data.mongodb.uri:NOT_FOUND}") String mongoUri
+    ) {
+        return args -> {
+            System.out.println("=================================");
+            System.out.println("MongoDB URI: " + mongoUri);
+            System.out.println("=================================");
+        };
+    }
     @Bean
     public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
         return new MongoTransactionManager(dbFactory);
