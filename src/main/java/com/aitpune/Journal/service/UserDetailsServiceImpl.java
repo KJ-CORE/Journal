e4d44ce18e7a1c82b1933936f4a19e4cc06f38f6
KJ-CORE <kunaljha210@outlook.in>
2026-01-30 23:18:24 +0530

    29/01/2026

--- src/main/java/com/aitpune/Journal/service/UserDetailsServiceImpl.java
+++ src/main/java/com/aitpune/Journal/service/UserDetailsServiceImpl.java
@@ -15,7 +15,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
-    @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByusername(username);
         if(user != null){
--- src/test/java/com/aitpune/Journal/service/UserDetailServiceImplTest.java
+++ src/test/java/com/aitpune/Journal/service/UserDetailServiceImplTest.java
@@ -1,4 +1,32 @@
 package com.aitpune.Journal.service;
 
+import com.aitpune.Journal.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 public class UserDetailServiceImplTest {
+
+    @MockitoBean
+    private UserDetailsServiceImpl userDetailsService;
+
+    @MockitoBean
+    private UserRepository userRepository;
+
+    @Test
+    void loadbyusername(){
+        when(userDetailsService.loadUserByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("Ram").password("asdasc").roles("USER").build());
+
+        UserDetails userDetails = userDetailsService.loadUserByusername("ram");
+        Assertions.assertNotNull(userDetails);
+    }
+
 }
